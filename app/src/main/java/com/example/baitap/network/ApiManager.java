2506ea5520c27ec6441e688a2071d299e80009aa
diepminhtuan <tuandmth2007019@fpt.edu.vn>
2022-07-01 11:43:37 +0700

    a

--- app/src/main/java/com/example/baitap/network/ApiManager.java
+++ app/src/main/java/com/example/baitap/network/ApiManager.java
@@ -8,12 +8,12 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiManager {
-    public static String BASE_URL = "https://developer.accuweather.com";
+    public static String BASE_URL = "http://dataservice.accuweather.com";
 
     @GET("/forecasts/v1/hourly/12hour/353412?apikey=fAr0sVK2d9dXtwsygNT3BMEKA4BaljQ5&language=vi-vn&metric=true")
     Call<List<Wheather>> gethour();
 
-    @GET("/forecasts/v1/daily/5day/353412?apikey=i5doO0KFzjMo7e7tsZWcyeHVwhEV9yCE&language=vi-vn&metric=true")
+    @GET("/forecasts/v1/daily/5day/353412?apikey=fAr0sVK2d9dXtwsygNT3BMEKA4BaljQ5&language=vi-vn&metric=true")
     Call<List<Wheather>> getday();
 
 }
